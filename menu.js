266b1422f7611fa96581bb3bb2c1469884818ffc
Giullya Gomes <gomes.giullyav@gmail.com>
2024-11-19 00:00:49 -0300

    Subindo o projeto para o github com o escolpo semi pronto porém com alguns bugs

---
+++ menu.js
@@ -0,0 +1,24 @@
+let btnAbrir = document.getElementById('btn-abrir')
+let menu = document.getElementById('menu-mobile')
+
+btnAbrir.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+let inicio = document.getElementById('inicio')
+
+let btnContato = document.getElementById('btn-contato')
+let formulario = document.getElementById('fale-comigo')
+
+let habilidades = document.getElementById('habilidades')
+
+let sobre = document.getElementById('sobre')
+
+let projetos = document.getElementById('projetos')
+
+// Função para ir ao início
+
+// Função para ir as habilidades
+// Função para ir ao sobre
+// Função para ir aos projetos
+// Função para ir ao formulário
